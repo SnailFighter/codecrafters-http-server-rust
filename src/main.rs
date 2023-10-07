21faaa4a69b521d4e1b9638238cecd93b33790b9
owen.chao <owen.chao@bamboonetworks.com>
2023-10-07 17:38:49 +0800

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::net::TcpListener;
 use std::io::Write;
 
+
 fn main(){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
